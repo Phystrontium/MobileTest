1f6463c3e4ae58f1eb795aecefbe9ae14a57fee2
Aaron Reynolds <aaronreynolds@synics.co>
2016-05-15 20:08:09 -0400

    More Graphics Settings
    Added more settings for the graphics.

---
+++ Source/MobileTest/TestClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MobileTest.h"
+#include "TestClass.h"
+
+TestClass::TestClass()
+{
+}
+
+TestClass::~TestClass()
+{
+}
